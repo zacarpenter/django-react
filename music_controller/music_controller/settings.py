31fa2b05c6ce34ebe830e0d42d36aa2f97643a3b
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-08-22 16:15:03 -0400

    added frontend view with test output

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
