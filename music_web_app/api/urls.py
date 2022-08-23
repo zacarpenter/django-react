13755040f554d363b3c6b7b7eb1dd45f06861138
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-08-23 17:05:19 -0400

    styled the createroompage, added create-room url

--- music_web_app/api/urls.py
+++ music_web_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 # If URL is blank, call main
 # Return main as an HttpResponse
 
 urlpatterns = [
-    path('room', RoomView.as_view())
+    path('room', RoomView.as_view()),
+     path('create-room', CreateRoomView.as_view())
 ]
