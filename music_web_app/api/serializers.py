336ef16ab88233b2818218d42d99bcf0208e081e
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-08-23 14:42:05 -0400

    modified createroomview to verify data in requests

--- music_web_app/api/serializers.py
+++ music_web_app/api/serializers.py
@@ -6,3 +6,9 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
+# ensure data sent in POST request is valid
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
