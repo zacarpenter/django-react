d43beac01d7a1ae6e768ec9e8fd8b4f210fd4b34
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-08-26 13:02:33 -0400

    added leave room button

--- music_web_app/api/urls.py
+++ music_web_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
 
 # If URL is blank, call main
 # Return main as an HttpResponse
@@ -9,5 +9,6 @@ urlpatterns = [
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
-    path('user-in-room', UserInRoom.as_view())
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view())
 ]
--- music_web_app/frontend/src/components/Room.js
+++ music_web_app/frontend/src/components/Room.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Grid, Button, Typography } from "@mui/material";
 
 export default class Room extends Component {
   constructor(props) {
@@ -9,10 +10,11 @@ export default class Room extends Component {
       isHost: false,
     };
     this.roomCode = this.props.match.params.roomCode;
-    this.getRoomDetails();
+    this._getRoomDetails();
+    this._leaveButtonPressed = this._leaveButtonPressed.bind(this);
   }
 
-  getRoomDetails() {
+  _getRoomDetails() {
     fetch("/api/get-room" + "?code=" + this.roomCode)
       .then((response) => response.json())
       .then((data) => {
@@ -24,14 +26,56 @@ export default class Room extends Component {
       });
   }
 
+  _leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) =>
+      this.props.history.push("/")
+    );
+  }
+
   render() {
     return (
-      <div>
-        <h3>{this.roomCode}</h3>
-        <p>Votes: {this.state.votesToSkip}</p>
-        <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-        <p>Host: {this.state.isHost.toString()}</p>
-      </div>
+      <Grid container spacing={1} alignItems="center" direction="column">
+        <Grid item xs={12}>
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6" component="h6">
+            Votes: {this.state.votesToSkip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6" component="h6">
+            Guest Can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6" component="h6">
+            Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            color="error"
+            variant="contained"
+            onClick={this._leaveButtonPressed}
+          >
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
+
+      // <div>
+      //   <h3>{this.roomCode}</h3>
+      //   <p>Votes: {this.state.votesToSkip}</p>
+      //   <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+      //   <p>Host: {this.state.isHost.toString()}</p>
+      // </div>
     );
   }
 }
