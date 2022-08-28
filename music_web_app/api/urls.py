686399379d12b63701fe8fc242ef24f10cc635f4
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-08-28 16:27:01 -0400

    fixed class name

--- music_web_app/api/urls.py
+++ music_web_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom, UpdateRoom
 
 # If URL is blank, call main
 # Return main as an HttpResponse
@@ -10,5 +10,6 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', UserInRoom.as_view()),
-    path('leave-room', LeaveRoom.as_view())
+    path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view()),
 ]
